f2ccaacfd129f0f9135dc4186214b2767b8bccad
Nikita Lysov <nlof142@gmail.com>
2021-05-27 19:35:43 +0700

    Добавьте файлы проекта.

---
+++ WebApplication9/Models/Doctors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication9.Models
+{
+    public class Doctors
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string speciality { get; set; }
+    }
+}
---
+++ WebApplication9/Models/Hospitals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication9.Models
+{
+    public class Hospitals
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "waiting name")]
+        [Display(Name = "Name")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "waiting adress")]
+        [Display(Name = "Adress")]
+        public string adress { get; set; }
+
+        [Required(ErrorMessage = "waiting phone")]
+        [Display(Name = "Phones")]
+        //[RegularExpression(@"^(?([0-9]{3}))?[-. ]?([0-9]{3})[-. ]?([0-9]{5})$", ErrorMessage = "Wrong phone")]
+        [DataType(DataType.PhoneNumber)]
+        public string phones { get; set; }
+    }
+}
